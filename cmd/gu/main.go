e86c626ad6a511a59006f40256408d39a11f5970
bzub <bzubrod@gmail.com>
2018-01-17 21:54:13 -0600

    Update metrics.Emit() usage, add missing assets generator

--- cmd/gu/main.go
+++ cmd/gu/main.go
@@ -9,11 +9,12 @@ import (
 	"strings"
 
 	"github.com/gu-io/gu/generators"
+	"github.com/influx6/faux/fmtwriter"
 	"github.com/influx6/faux/metrics"
 	"github.com/influx6/faux/metrics/custom"
-	"github.com/influx6/moz/annotations"
 	"github.com/influx6/moz/ast"
 	"github.com/influx6/moz/gen"
+	"github.com/influx6/moz/gen/templates"
 	cli "gopkg.in/urfave/cli.v2"
 )
 
@@ -298,17 +299,19 @@ func initCommands() {
 			generators.RegisterGenerators(register)
 
 			// Register @assets annotation for our registery as well.
-			register.Register("assets", annotations.AssetsAnnotationGenerator)
+			register.Register("assets", AssetsAnnotationGenerator)
 
 			events := metrics.New(custom.BlockDisplay(os.Stdout))
 			pkg, err := ast.ParseAnnotations(events, indir)
 			if err != nil {
-				events.Emit(metrics.Error(err).With("dir", indir).With("message", "Failed to parse package annotations"))
+				events.Emit(metrics.Error(err), metrics.With("dir", indir),
+					metrics.Message("Failed to parse package annotations"))
 				return err
 			}
 
-			if err := ast.Parse("", events, register, false, pkg...); err != nil {
-				events.Emit(metrics.Error(err).With("dir", indir).With("message", "Failed to parse package annotations"))
+			if err := ast.Parse(indir, events, register, false, pkg...); err != nil {
+				events.Emit(metrics.Error(err), metrics.With("dir", indir),
+					metrics.Message("Failed to parse package annotations"))
 				return err
 			}
 
@@ -371,4 +374,97 @@ func writeFile(targetFile string, data []byte) error {
 	}
 
 	return nil
+}
+
+// AssetsAnnotationGenerator defines a package level annotation generator which builds a go package in
+// root of the package it appears in to provide a means to quickly draft all file contents into the created
+// package.
+// Annotation: @assets
+// Arguments(Optional): (PackageName, FileExtensionsToSupport, DirectorNameForFiles)
+// 	e.g @assets(assets, ".tml : .bol : .go : .js", mytemplates).
+func AssetsAnnotationGenerator(toDir string, an ast.AnnotationDeclaration, pkgDeclr ast.PackageDeclaration, pkg ast.Package) ([]gen.WriteDirective, error) {
+	var directives []gen.WriteDirective
+
+	var extensions []string
+
+	pkgName := "assets"
+	contentFileName := "files"
+
+	if argLen := len(an.Arguments); argLen != 0 {
+		pkgName = an.Arguments[0]
+
+		if argLen > 1 {
+			extensions = strings.Split(an.Arguments[1], ":")
+		}
+
+		if argLen > 2 {
+			contentFileName = an.Arguments[2]
+		}
+	}
+
+	genFile := gen.Package(
+		gen.Name(pkgName),
+		gen.Text("//go:generate go run generate.go"),
+	)
+
+	directives = append(directives, gen.WriteDirective{
+		Writer:   fmtwriter.New(genFile, true, true),
+		FileName: fmt.Sprintf("%s.go", pkgName),
+		Dir:      pkgName,
+	})
+
+	directives = append(directives, gen.WriteDirective{
+		Dir: filepath.Join(pkgName, contentFileName),
+	})
+
+	mainFile := gen.Block(
+		gen.Commentary(
+			gen.Text("+build ignore"),
+		),
+		gen.Text("\n"),
+		gen.Text("\n"),
+		gen.Package(
+			gen.Name("main"),
+			gen.Imports(
+				gen.Import("fmt", ""),
+				gen.Import("path/filepath", ""),
+				gen.Import("github.com/influx6/moz/gen", ""),
+				gen.Import("github.com/influx6/moz/utils", ""),
+				gen.Import("github.com/influx6/faux/vfiles", ""),
+				gen.Import("github.com/influx6/faux/fmtwriter", ""),
+				gen.Import("github.com/influx6/faux/metrics", ""),
+				gen.Import("github.com/influx6/faux/metrics/custom", ""),
+			),
+			gen.Function(
+				gen.Name("main"),
+				gen.Constructor(),
+				gen.Returns(),
+				gen.Block(
+					gen.SourceText(
+						string(templates.Must("assets/assets.tml")),
+						struct {
+							Extensions       []string
+							TargetDir        string
+							Package          string
+							GenerateTemplate string
+						}{
+							TargetDir:  contentFileName,
+							Extensions: extensions,
+							Package:    pkgName,
+							GenerateTemplate: `{{range $key, $value := .Files}}files[{{quote $key}}] = []byte("{{$value}}")
+							{{end}}`,
+						},
+					),
+				),
+			),
+		),
+	)
+
+	directives = append(directives, gen.WriteDirective{
+		Writer:   fmtwriter.New(mainFile, true, true),
+		FileName: "generate.go",
+		Dir:      pkgName,
+	})
+
+	return directives, nil
 }
